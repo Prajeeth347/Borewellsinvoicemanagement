e6bb33ea20d148b3279729bf822d08595c33a0cb
Prajeeth <prajeeth347@gmail.com>
2021-11-05 16:07:26 +0530

    PDF opener removal

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/navigation_toolbar.dart';
-import 'package:intl/intl.dart';
 import 'package:sanjay/Casing.dart';
 import 'package:sanjay/viewbill.dart';
 
